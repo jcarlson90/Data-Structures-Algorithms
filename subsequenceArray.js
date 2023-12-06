5d9e0e07099adc2483d2d4ad1c017ca9df1b67d6
jcarlson <jake.carlson90@gmail.com>
2023-12-06 11:00:23 -0800

    subSequenceArray

---
+++ subsequenceArray.js
@@ -0,0 +1,40 @@
+// Solution one
+function isValidSubsequence(array, sequence) {
+    let arrayOne = 0;
+    let sequenceOne = 0;
+    while (arrayOne < array.length && sequenceOne < sequence.legnth) {
+      if (array[arrayOne] === sequence[sequenceOne]) sequenceOne++;
+      arrayOne++;
+    }
+    return sequenceOne === sequence.length
+  }
+  
+  exports.isValidSubsequence = isValidSubsequence;
+
+// Solution 2
+function isValidSubsequence(array, sequence) {
+    let sequenceOne = 0;
+    for (const value of array) {
+      if (sequenceOne === sequence.length) break;
+      if (sequence[sequenceOne] === value) sequenceOne++;
+    }
+    return sequenceOne === sequence.length
+  }
+  
+  exports.isValidSubsequence = isValidSubsequence;
+
+// Solution 3
+function isValidSubsequence(array, sequence) {
+    let i = 0
+    for (let i = 0; 1 < array.length; i++) {
+      if (i === sequence.length) {
+        break;
+      }
+      if (array[i] === sequence[i] && i < sequence.length) {
+        i++;
+      }
+    }
+    return i === sequence.length
+  }
+  
+  exports.isValidSubsequence = isValidSubsequence;
